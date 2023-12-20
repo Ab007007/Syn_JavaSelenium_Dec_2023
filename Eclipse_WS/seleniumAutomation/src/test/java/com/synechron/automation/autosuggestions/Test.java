cf2c3ae529fb3ca02a040a7db18c70b35b03b836
Ab007007 <aru03.info@gmail.com>
2023-12-20 15:11:48 +0530

    selenium first code

---
+++ Eclipse_WS/seleniumAutomation/src/test/java/com/synechron/automation/autosuggestions/Test.java
@@ -0,0 +1,8 @@
+package com.synechron.automation.autosuggestions;
+
+public class Test {
+
+		public static void main(String[] args) {
+			System.out.println("ggg");
+		}
+}
--- Eclipse_WS/seleniumAutomation/src/test/java/com/synechron/automation/basic/FormFillDemo.java
+++ Eclipse_WS/seleniumAutomation/src/test/java/com/synechron/automation/basic/FormFillDemo.java
@@ -3,33 +3,32 @@ package com.synechron.automation.basic;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FormFillDemo {
 
-	
-	@Test
-	public void simpleTest() throws InterruptedException 
-	{
-	//	System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+	public static void main(String[] args) throws InterruptedException 	{
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 	//	WebDriver driver = new ChromeDriver();
-		WebDriverManager.chromedriver().setup();
+	//	WebDriverManager.chromedriver().browserVersion("120.0.6099.110").setup();
 		WebDriver driver = new ChromeDriver();
 		
 		driver.get("http://formy-project.herokuapp.com/form");
-		
+		Thread.sleep(5000);
 		driver.findElement(By.id("first-name")).sendKeys("Aravinda");
+		Thread.sleep(2000);
 		driver.findElement(By.id("last-name")).sendKeys("H");
+		Thread.sleep(2000);
 		driver.findElement(By.id("job-title")).sendKeys("Trainer");
-		
+		Thread.sleep(2000);
 		driver.findElement(By.id("radio-button-3")).click();
+		Thread.sleep(2000);
 		driver.findElement(By.id("checkbox-1")).click();
 		
 		driver.findElement(By.linkText("Submit")).click();
 		
-		Thread.sleep(2000);
+		Thread.sleep(5000);
 		
 		String successMessage = driver.findElement(By.xpath("//div[@role='alert']")).getText();
 		
